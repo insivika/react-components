850285fb218a09e18f52c44c3214a75843685409
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-08-11 15:29:42 -0700

    Set up TodoList

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Reactly', 'Maybe Sleep']}/>
+  </div>
+);
+
+
+
+var TodoList = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
