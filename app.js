3d665cc42acf1c587985a8b7d73530331c6adbff
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-08-11 18:50:28 -0700

    Improved UI by changin colors and font sizes and added input and submit fields.

--- app.js
+++ app.js
@@ -1,20 +1,98 @@
 var App = () => (
   <div>
-    <h2>My Todo List</h2>
+    <h2>My Todo List</h2><AddItemInput/><AddItemButton />
     <TodoList todos={['Learn React', 'Crush Reactly', 'Maybe Sleep']}/>
+
   </div>
 );
+
+class AddItemInput extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {value: ''};
+
+    }
+
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+
+  render(){
+    return (
+      <input value={this.state.value} onChange={this.handleChange.bind(this)} type="text"/>
+    )
+  }
+};
+
+class AddItemButton extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  onButtonClicked(){
+    console.log('button has been clicked')
+  }
+
+  render(){
+
+    return(
+
+      <input onClick={this.onButtonClicked.bind(this)} className="add-btn" value="add" type="submit" />
+
+    );
+
+  }
+}
+
+
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      done: false,
+      hovered: false,
+    }
+
+  }
+
+  onListItemHovered(){
+    this.setState({
+      hovered: !this.state.hovered
+    })
+  }
+
+  onListItemClicked() {
+    this.setState({
+      done: !this.state.done
+    })
+  }
+
+  render() {
+
+    var hoverEffect = this.state.hovered ? 'list-item-hovered list-item' : 'list-item'
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+
+    return (
+      <div className={hoverEffect} style={style} onMouseEnter={this.onListItemHovered.bind(this)} onMouseLeave={this.onListItemHovered.bind(this)} onClick={this.onListItemClicked.bind(this)}>{ this.props.todo }</div>
+    );
+  }
+}
 
 
 
 var TodoList = (props) => (
-  <ul>
-    <li>{props.todos[0]}</li>
-    <li>{props.todos[1]}</li>
-    <li>{props.todos[2]}</li>
-  </ul>
-);
-
+  <div>
+    {props.todos.map(todo =>
+      < TodoListItem todo={todo} />
+    )}
+  </div>
+)
 
 ReactDOM.render(
   <App />,
